50053927241344a3a5020af789fe9425918f5fb5
AHMAD9853 <84655050+AHMAD9853@users.noreply.github.com>
2022-09-13 13:54:29 +0530

    Add files via upload

---
+++ First program/literals.java
@@ -0,0 +1,16 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 23;
+        short age3 = 56;
+        long dinoage = 62212213L;
+        char ch = 'U';
+        float f1 = 5.6f;
+        double d1 =4.66;
+        boolean a = true;
+        boolean b = false;
+        System.out.println(age2);
+        String str = "ahmad";
+        System.out.println(str);
+    }
+}
---
+++ First program/src/com/addthreenumber/Main.java
@@ -0,0 +1,14 @@
+package com.addthreenumber;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("helloworld");
+          int a = 2;
+          int b = 4;
+          int c = 7;
+          int sum =  a + b + c;
+        System.out.println(sum);
+    }
+}
